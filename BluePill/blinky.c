3a97882a6a7cf2b2ee08aa9b7925142ffcf4eb25
John Honniball <coredump@virginmedia.com>
2022-12-19 12:04:22 +0000

    Configure UART1 TxD and RxD pins.
    Configure alternate functions for PA9 and PA10.

--- BluePill/blinky.c
+++ BluePill/blinky.c
@@ -40,8 +40,8 @@ void SysTick_Handler(void)
 
 static void t1ou(const int ch)
 {
-   // Wait for TX ready
-   while ((USART1->SR & 1<<7) == 0)
+   // Wait for TX empty
+   while ((USART1->SR & USART_SR_TXE) == 0)
       ;
   
    // Send byte
@@ -61,7 +61,7 @@ static void initMCU(void)
 static void initGPIOs(void)
 {
    // Configure Reset and Clock Control
-   RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;                    // Enable clock to GPIO peripherals on APB2 bus
+   RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;                    // Enable clock to GPIO C peripherals on APB2 bus
    
    // Configure PC13, the GPIO pin with the LED
    GPIOC->CRH &= ~(GPIO_CRH_MODE13 | GPIO_CRH_CNF13);     // Set PC13 to push-pull output mode
@@ -77,15 +77,21 @@ static void initGPIOs(void)
 
 static void initUARTs(void)
 {
-   // TODO: set alternate mode on pins PA9 and PA10
+   // Configure PA9, the GPIO pin with alternative function TxD1
+   GPIOA->CRH &= ~(GPIO_CRH_MODE9 | GPIO_CRH_CNF9);       // Clear configuration bits for PA9
+   GPIOA->CRH |= (GPIO_CRH_CNF9_1 | GPIO_CRH_MODE9_0 | GPIO_CRH_MODE9_1);   // Configure PA9 as alternate function, push-pull
+   
+   // Configure PA10, the GPIO pin with alternative function RxD1
+   GPIOA->CRH &= ~(GPIO_CRH_MODE10 | GPIO_CRH_CNF10);     // Clear configuration bits for PA10
+   GPIOA->CRH |= GPIO_CRH_CNF9_1;                         // Configure PA9 as alternate function, floating input
    
    // Configure the UART - defaults are 1 start bit, 8 data bits, 1 stop bit,
    // no parity, so I'll leave those as they are
-   RCC->APB2ENR |= 1<<14;         // Enable USART1 clock
-   USART1->CR1 |= 1<<13;          // Switch on the UART
-   USART1->BRR |= 0x683;          // Set for 9600 baud (reference manual page 528)
-   USART1->CR1 |= 1<<3;           // Enable transmitter (sends a junk character)
-// USART1->CR1 |= 1<<2;           // Enable receiver
+   RCC->APB2ENR |= RCC_APB2ENR_USART1EN;  // Enable USART1 clock
+   USART1->CR1 |= USART_CR1_TE;           // Switch on the UART
+   USART1->BRR |= 0x683;                  // Set for 9600 baud (reference manual page 528)
+   USART1->CR1 |= USART_CR1_TE;           // Enable transmitter (sends a junk character)
+// USART1->CR1 |= USART_CR1_RE;           // Enable receiver
 }
 
 
