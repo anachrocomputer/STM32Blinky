4d4ac69485ec6a19e77debc9a56c230d630d3673
John Honniball <coredump@virginmedia.com>
2022-12-21 01:45:52 +0000

    Fix both UARTs.
    Switch on clock to GPIO port A, so that we can set it up correctly for Alternate Functions on PA9/10 and PA2/3. Enable both UART clocks. Fix error in CR1 setting bit USART_CR1_UE. Adjust baud rate registers to get 9600 baud on both channels. Restore function calls that send characters. Add 'STM32' at RESET. TODO: parameterise setting of baud rates and find out why we need to set a different number from that in the data sheet on UART1.

--- BluePill/blinky.c
+++ BluePill/blinky.c
@@ -114,9 +114,13 @@ static void initGPIOs(void)
 
 static void initUARTs(void)
 {
+   RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;                    // Enable clock to GPIO A peripherals on APB2 bus
+   RCC->APB2ENR |= RCC_APB2ENR_USART1EN;                  // Enable USART1 clock
+   RCC->APB1ENR |= RCC_APB1ENR_USART2EN;                  // Enable USART2 clock
+   
    // Configure PA9, the GPIO pin with alternative function TxD1
    GPIOA->CRH &= ~(GPIO_CRH_MODE9 | GPIO_CRH_CNF9);       // Clear configuration bits for PA9
-   GPIOA->CRH |= (GPIO_CRH_CNF9_1 | GPIO_CRH_MODE9_0 | GPIO_CRH_MODE9_1);   // Configure PA9 as alternate function, push-pull
+   GPIOA->CRH |= GPIO_CRH_CNF9_1 | GPIO_CRH_MODE9_0 | GPIO_CRH_MODE9_1;     // Configure PA9 as alternate function, push-pull
    
    // Configure PA10, the GPIO pin with alternative function RxD1
    GPIOA->CRH &= ~(GPIO_CRH_MODE10 | GPIO_CRH_CNF10);     // Clear configuration bits for PA10
@@ -124,25 +128,22 @@ static void initUARTs(void)
    
    // Configure PA2, the GPIO pin with alternative function TxD2
    GPIOA->CRL &= ~(GPIO_CRL_MODE2 | GPIO_CRL_CNF2);       // Clear configuration bits for PA2
-   GPIOA->CRL |= (GPIO_CRL_CNF2_1 | GPIO_CRL_MODE2_0 | GPIO_CRL_MODE2_1);   // Configure PA2 as alternate function, push-pull
+   GPIOA->CRL |= GPIO_CRL_CNF2_1 | GPIO_CRL_MODE2_0 | GPIO_CRL_MODE2_1;     // Configure PA2 as alternate function, push-pull
    
    // Configure PA3, the GPIO pin with alternative function RxD2
    GPIOA->CRL &= ~(GPIO_CRL_MODE3 | GPIO_CRL_CNF3);       // Clear configuration bits for PA3
    GPIOA->CRL |= GPIO_CRL_CNF3_1;                         // Configure PA3 as alternate function, floating input
    
    // Configure UART1 - defaults are 1 start bit, 8 data bits, 1 stop bit, no parity
-   RCC->APB2ENR |= RCC_APB2ENR_USART1EN;  // Enable USART1 clock
-   USART1->CR1 |= USART_CR1_TE;           // Switch on the UART
-   USART1->BRR |= 0x683;                  // Set for 9600 baud (reference manual page 528)
+   USART1->CR1 |= USART_CR1_UE;           // Switch on the UART
+   USART1->BRR |= (467<<4) | 12;          // Set for 9600 baud (reference manual page 799) 72000000 / (16 * 9600)
    USART1->CR1 |= USART_CR1_TE;           // Enable transmitter (sends a junk character)
 // USART1->CR1 |= USART_CR1_RE;           // Enable receiver
 
    // Configure UART2 - defaults are 1 start bit, 8 data bits, 1 stop bit, no parity
-   // PCLK1 not set up yet, so it doesn't work
-   //RCC->APB1ENR |= RCC_APB1ENR_USART2EN;  // Enable USART2 clock
-   //USART2->CR1 |= USART_CR1_TE;           // Switch on the UART
-   //USART2->BRR |= 0x683;                  // Set for 9600 baud (reference manual page 528)
-   //USART2->CR1 |= USART_CR1_TE;           // Enable transmitter (sends a junk character)
+   USART2->CR1 |= USART_CR1_UE;           // Switch on the UART
+   USART2->BRR |= (234<<4) | 6;           // Set for 9600 baud (reference manual page 799) 36000000 / (16 * 9600)
+   USART2->CR1 |= USART_CR1_TE;           // Enable transmitter (sends a junk character)
 // USART2->CR1 |= USART_CR1_RE;           // Enable receiver
 }
 
@@ -178,22 +179,28 @@ int main(void)
    
    __enable_irq();   // Enable all interrupts
    
+   t1ou('S');
+   t1ou('T');
+   t1ou('M');
+   t1ou('3');
+   t1ou('2');
+   
    while (1) {
       GPIOC->BSRR = GPIO_BSRR_BR13; // GPIO pin PC13 LOW, LED on
       
       for (dally = 0; dally < 2400000; dally++)
          ;
       
-      //t1ou('-');
-      //t1ou2('A');
+      t1ou('-');
+      t1ou2('A');
       
       GPIOC->BSRR = GPIO_BSRR_BS13; // GPIO pin PC13 HIGH, LED off
       
       for (dally = 0; dally < 2400000; dally++)
          ;
       
-      //t1ou('*');
-      //t1ou2('B');
+      t1ou('*');
+      t1ou2('B');
    }
 }
 
